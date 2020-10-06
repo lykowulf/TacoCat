3d8986e5945ee808f9a06e9a25eda60fb6178744
lykowulf <josephpgreen@gmail.com>
2020-10-06 19:36:39 -0400

    everything works now

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -4,23 +4,23 @@
 //    palindrome();
 //});
 
-function palindrome() {
-    let input = document.getElementById("input1").value;
-    let reverse = ""
-    let cutWord = input.split("").reverse().join("");
+//function palindrome() {
+//    let input = document.getElementById("input1").value;
+//    let reverse = ""
+//    let cutWord = input.split("").reverse().join("");
 
 
-    for (i = input; i <= input.length - 1; i--) {
-        reverse += input.charAt(i);
-        document.getElementById("flipResult").innerHTML = cutWord;
-    }
-    if (input == reverse) {
-        alert("is Palindrome")
-    }
-    else {
-        alert("try again")
-    }
-}
+//    for (i = input; i <= input.length - 1; i--) {
+//        reverse += input.charAt(i);
+//        document.getElementById("flipResult").innerHTML = cutWord;
+//    }
+//    if (input == reverse) {
+//        alert("is Palindrome")
+//    }
+//    else {
+//        alert("try again")
+//    }
+//}
 
 
 
@@ -30,3 +30,20 @@ function palindrome() {
 //})
 
 //document.getElementById("reverse the out put").innerHTML = reverse
+
+function palindrome() {
+    let inpuWord = document.getElementById("input1").value;
+    let reverseWord = "";
+    for (let loop = inpuWord.length - 1; loop >= 0; loop--) {
+        reverseWord += inpuWord.charAt(loop);
+    }
+    let palResult = inpuWord.split("").reverse().join("");
+    if (inpuWord == reverseWord) {
+        document.getElementById("flipResult").innerHTML = palResult;
+        alert("you're correct!!")
+    }
+    else {
+        alert("Wrong");
+    }
+    
+}
