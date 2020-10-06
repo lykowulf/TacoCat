0b07d2e7282dac8bfa837d5d0c4547f09706085d
lykowulf <josephpgreen@gmail.com>
2020-10-06 15:40:42 -0400

    Initial commit

---
+++ Scripts/custom.js
@@ -0,0 +1,28 @@
+﻿//reverse a string/Palindrome
+function reverseString() {
+  /*  //get data from user
+    let userInput = document.getElementById("reverse");
+
+
+//for loop to reverse the word
+    let reverse = "";
+
+    for (let loop = 0; loop < userInput.length - 1; loop--) {
+        reverse += userInput[loop];
+    }
+
+    //output to the user
+    if (inputword == reverse) {
+        alert("is palindrome")
+    }*/
+    let inputWord = document.getElementById("userInput").value;
+    let lowerInput = inputWord.toLowerCase();
+    let reverseWord = "";
+
+    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
+        lowerInput == reverseWord
+    }
+    //   /[\W_]/g
+
+}
+
