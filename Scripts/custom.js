9db1ec20d8540849d483ad6242f08ec3c3ff1fa3
lykowulf <josephpgreen@gmail.com>
2020-10-09 11:38:54 -0400

    changing the templete part 1

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,35 +1,4 @@
-﻿//reverse a string/Palindrome
-
-//document.getElementById("palindrome").addEventListener("click", function () {
-//    palindrome();
-//});
-
-//function palindrome() {
-//    let input = document.getElementById("input1").value;
-//    let reverse = ""
-//    let cutWord = input.split("").reverse().join("");
-
-
-//    for (i = input; i <= input.length - 1; i--) {
-//        reverse += input.charAt(i);
-//        document.getElementById("flipResult").innerHTML = cutWord;
-//    }
-//    if (input == reverse) {
-//        alert("is Palindrome")
-//    }
-//    else {
-//        alert("try again")
-//    }
-//}
-
-
-
-//   /[\W_]/g
-//document.getElementById("flippy").addEventListener("click", function () {
-
-//})
-
-//document.getElementById("reverse the out put").innerHTML = reverse
+﻿
 
 function palindrome() {
     let inpuWord = document.getElementById("input1").value;
@@ -49,13 +18,6 @@ function palindrome() {
 }
 
 
-
-
-
-//button eventlistener for tacocat
-//document.getElementById("btnTaco").addEventListener("click", function () {
-//}
-//);
 
 
 //user validation 
