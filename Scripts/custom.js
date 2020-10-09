c55d46d24d97c64a5667f93ee581f931e43f30ca
lykowulf <josephpgreen@gmail.com>
2020-10-09 14:22:31 -0400

    reformated code in order to read more clearly

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,4 +1,4 @@
-﻿
+﻿//determine if the word is a palindrome
 
 function palindrome() {
     let inpuWord = document.getElementById("input1").value;
@@ -13,8 +13,8 @@ function palindrome() {
     else {
         alert("Wrong");
     }
-        document.getElementById("flipResult").innerHTML = reverseWord;
-    
+    document.getElementById("flipResult").innerHTML = reverseWord;
+
 }
 
 
@@ -22,27 +22,44 @@ function palindrome() {
 
 //user validation 
 //press the button
-
-function isAlphaKey() {
-    // assign a value to character
-    //value assigned will be evt.which or evt.KeyCode
-    //if(evt.which)== true use evt.which else evt.KeyCode
-    var character = evt.which;
-
-    // i will allow what you types into the text box if its either a-z or A-Z
-    if (character > 97 && character < 122 || character >= 65 && character <= 90 || character == 8) {
-        return true;
-    }
-    else {
-        return false;
-    }
-
-}
-//need onkeypress="()" on the input field
+//document.getElementById("input1").addEventListener("keydown", function (e) {
+//    let char = e.which ? e.which : e.keyCode;
+//    if (char >= 97 && char <= 127
+//        || char >= 65 && char <= 90
+//        || char == 8
+//        || char >= 96 && char <= 105) {
+//        return true;
+//    } else {
+//        e.preventDefault();
+//        return false;
+//    }
+//});
 
 
-//first wire up an event listener that listens for the onkeypress event checks for valid input type i.e. numbers in this case. 
+//function isAlphaKey() {
+//    // assign a value to character
+//    //value assigned will be evt.which or evt.KeyCode
+//    //if(evt.which)== true use evt.which else evt.KeyCode
+//    var character = evt.which;
 
-document.getElementById("textTacoIn").addEventListener("keydown", function () {
+//    // i will allow what you types into the text box if its either a-z or A-Z
+//    if (character > 97 && character < 122 || character >= 65 && character <= 90 || character == 8) {
+//        return true;
+//    }
+//    else {
+//        return false;
+//    }
 
-});
+//}
+////need onkeypress="()" on the input field
+
+
+////first wire up an event listener that listens for the onkeypress event checks for valid input type i.e. numbers in this case. 
+
+//document.getElementById("textTacoIn").addEventListener("keydown", function () {
+
+//});
+
+
+
+
